5648007356905641b27de38ec1735530f6d26470
Josh Lamontagne <josh@joshlamontagne.com>
2015-03-10 16:19:00 -0400

    Update momentjs to v2.9.0

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'ember-cli-cal',
     included: function(app) {
         this._super.included(app);
-        
-        app.import(app.bowerDirectory + '/momentjs/moment.js');
+
+        app.import(app.bowerDirectory + '/moment/moment.js');
         app.import(app.bowerDirectory + '/lodash/dist/lodash.js');
         app.import('vendor/ember-cli-cal.css');
     }
