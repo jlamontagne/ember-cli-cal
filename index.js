3d0a24c84141aaf69a18416ef6a406c62f77bd7e
Josh Lamontagne <josh@joshlamontagne.com>
2015-03-11 23:22:19 -0400

    Remove lodash dependency

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
         this._super.included(app);
 
         app.import(app.bowerDirectory + '/moment/moment.js');
-        app.import(app.bowerDirectory + '/lodash/dist/lodash.js');
         app.import('vendor/ember-cli-cal.css');
     }
 };
